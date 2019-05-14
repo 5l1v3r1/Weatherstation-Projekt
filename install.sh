fcdddafcf48c6f77873d971e5fef441eab329255
Silky <40408435+S1lkys@users.noreply.github.com>
2019-05-14 14:57:25 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt install apache2
+apt install php
+pip install requests json subprocess sys
+apt update
+apt upgrade
+
+service apache2 start
+chmod 777 *
+mv index.html /var/www/html
+mv test.php /var/www/html
+
